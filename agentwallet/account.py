eaaf82ec4976cc7374b6b19e0b1954316ee0e58f
kgrofelnik <kresimir.grofelnik@gmail.com>
2023-11-20 16:13:01 +0100

    Small improvement

--- agentwallet/account.py
+++ agentwallet/account.py
@@ -8,26 +8,26 @@ from .api_client import ApiClient
 
 class Account:
     def __init__(self, api_key: str) -> None:
-        self.api_key = api_key
+        self.api_client = ApiClient(api_key)
 
     @classmethod
     def from_key(cls, api_key: str) -> "Account":
         return cls(api_key)
 
     def get_wallet(self, wallet_uid: str) -> Optional[Wallet]:
-        response = ApiClient(self.api_key).get(f"wallets/{wallet_uid}/")
+        response = self.api_client.get(f"wallets/{wallet_uid}/")
         if response.get("wallet"):
             transactions = []
             raw_transactions = response["wallet"].pop("transactions")
             for rt in raw_transactions:
                 transactions.append(Transaction(**rt))
             return Wallet(
-                _api_key=self.api_key, transactions=transactions, **response["wallet"]
+                _api_client=self.api_client, transactions=transactions, **response["wallet"]
             )
 
     def get_wallets(self) -> List[Wallet]:
-        response = ApiClient(self.api_key).get("wallets")
+        response = self.api_client.get("wallets")
         wallets = []
         for w in response["wallets"]:
-            wallets.append(Wallet(_api_key=self.api_key, **w))
+            wallets.append(Wallet(_api_client=self.api_client, **w))
         return wallets
--- agentwallet/wallet.py
+++ agentwallet/wallet.py
@@ -7,7 +7,7 @@ from .transaction import Transaction
 
 @dataclass
 class Wallet:
-    _api_key: str = field(repr=False, compare=False)
+    _api_client: ApiClient = field(repr=False, compare=False)
     wallet_uid: str
     transactions: Optional[List[Transaction]] = None
     balance_usd_cents: Optional[int] = None
@@ -17,12 +17,12 @@ class Wallet:
             "transfer_to_user_email": transfer_to,
             "amount_usd_cents": amount_usd_cents,
         }
-        response = ApiClient(self._api_key).post(
+        response = self._api_client.post(
             f"wallet/{self.wallet_uid}/transfer", data=data
         )
         return response["response"] == "OK"
 
     def balance(self) -> int:
-        response = ApiClient(self._api_key).get(f"wallets/{self.wallet_uid}/")
+        response = self._api_client.get(f"wallets/{self.wallet_uid}/")
         self.balance_usd_cents = response["wallet"]["balance_usd_cents"]
         return self.balance_usd_cents
