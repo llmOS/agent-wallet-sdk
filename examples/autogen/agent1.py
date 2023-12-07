0c2a57ee3edf39150838756d450f2d670b3b8112
taivop <taivo@pungas.ee>
2023-12-07 15:04:12 +0200

    agent endpoint finally works

--- examples/autogen/agent1.py
+++ examples/autogen/agent1.py
@@ -22,13 +22,18 @@ async def get_api_key(api_key_header: str = Security(api_key_header)):
 # Initialize your assistant and user_proxy agents
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
-user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
+user_proxy = UserProxyAgent("user_proxy", human_input_mode="NEVER", max_consecutive_auto_reply=0, code_execution_config=False)
+
 
 @app.post("/chat", dependencies=[Depends(get_api_key)])
 async def chat_endpoint(request: ChatRequest):
     try:
         # Call user_proxy.initiate_chat with the request message
-        response = user_proxy.initiate_chat(assistant, message=request.message)
+        user_proxy.initiate_chat(assistant, message=request.message)
+        last_message = assistant.last_message()
+
+        response = last_message
+
         return response
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
